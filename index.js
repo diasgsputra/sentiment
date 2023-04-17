9591debc060c2e5334e8230f3359b4938725cb93
diasgsputra <diasgsputra@gmail.com>
2023-04-17 23:22:43 +0700

    sales-report

--- index.js
+++ index.js
@@ -55,6 +55,51 @@ app.get('/sentiment', (req, res) => {
     });
 });
 
+app.put('/sales-report', (req, res) => {
+        // buat variabel penampung data dan query sql
+        const data = { ...req.body };
+        const querySearch = 'SELECT * FROM sales-report WHERE id = ?';
+        const queryUpdate = 'UPDATE sales-report SET ? WHERE id = ?';
+    
+        // jalankan query untuk melakukan pencarian data
+        koneksi.query(querySearch, 1, (err, rows, field) => {
+            // error handling
+            if (err) {
+                return res.status(500).json({ message: 'Ada kesalahan', error: err });
+            }
+    
+            // jika id yang dimasukkan sesuai dengan data yang ada di db
+            if (rows.length) {
+                // jalankan query update
+                koneksi.query(queryUpdate, [data, 1], (err, rows, field) => {
+                    // error handling
+                    if (err) {
+                        return res.status(500).json({ message: 'Ada kesalahan', error: err });
+                    }
+    
+                    // jika update berhasil
+                    res.status(200).json({ success: true, message: 'Update success!' });
+                });
+            } else {
+                return res.status(404).json({ message: 'Data tidak ditemukan!', success: false });
+            }
+        });
+});
+
+
+// read data / get data
+app.get('/sales-report', (req, res) => {
+    const querySql = 'SELECT * FROM sales-report';
+
+    koneksi.query(querySql, (err, rows, field) => {
+        if (err) {
+            return res.status(500).json({ message: 'Ada kesalahan', error: err });
+        }
+
+        res.status(200).json({ success: true, data: rows });
+    });
+});
+
 app.listen(8000, () => {
 	console.log('server started on port 8000');
 });
